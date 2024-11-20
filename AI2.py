3d5b667ceb033eb7bdba08a14805a711c13c6033
parth <90976533+ParthBoyCoder@users.noreply.github.com>
2024-11-20 21:35:08 +0530

    Update AI2.py

--- AI2.py
+++ AI2.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyB-Tc04NNO_BcV84JhdJy3BdEi2fsraK08")
+genai.configure(api_key="key2")
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("How are you?")
